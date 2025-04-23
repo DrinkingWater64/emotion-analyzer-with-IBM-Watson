1192432f888cd92f747e156b75009d2ee65981f9
akshar <61174663+DrinkingWater64@users.noreply.github.com>
2025-04-23 13:54:42 +0600

    Added function for emotion detection

---
+++ emotion_detection.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    """
+    Detects the emotion of a text
+    :param text_to_analyse:
+    :return: The 'text' attribute of the response object, or None if an error occurs.
+    """
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyse}}
+    try:
+        response = requests.post(url, headers, input_json)
+        response.raise_for_status()
+        result = response.json()
+        return result['document']['emotion']['predictions'][0]['label']
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        return None
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return None
+    except KeyError as e:
+        print(e)
+        return None
+
