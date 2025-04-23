77397764474d3416c01d53fbebf674db31ea8f1f
akshar <61174663+DrinkingWater64@users.noreply.github.com>
2025-04-23 15:31:23 +0600

    fixed the test file with updated code

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -4,19 +4,23 @@ from EmotionDetection import emotion_detector
 class TestEmotionDetection(unittest.TestCase):
     def test_positive_joy(self):
         text = "I am glad this happened"
-        self.assertEqual(emotion_detector(text), 'joy')
+        result = emotion_detector(text)
+        self.assertEqual(result[0], 'joy')
 
     def test_negative_disgust(self):
         text = "I feel disgusted just hearing about this"
-        self.assertEqual(emotion_detector(text), 'disgust')
+        result = emotion_detector(text)
+        self.assertEqual(result[0], 'disgust')
 
     def test_negative_sadness(self):
         text = "I am so sad about this"
-        self.assertEqual(emotion_detector(text), 'sadness')
+        result = emotion_detector(text)
+        self.assertEqual(result[0], 'sadness')
 
     def test_negative_fear(self):
         text = "I am really afraid that this will happen"
-        self.assertEqual(emotion_detector(text), 'fear')
+        result = emotion_detector(text)
+        self.assertEqual(result[0], 'fear')
 
 if __name__ == '__main__':
     unittest.main()
